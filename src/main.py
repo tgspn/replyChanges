d6d0a66d5552c91accd5a9ce5a073227e80fafa6
jonas <jonas@itera.com.br>
2020-03-12 13:28:51 -0300

    get lines

--- src/main.py
+++ src/main.py
@@ -4,11 +4,22 @@ def read_input(file_path):
 	with open(file_path,'r') as f:
 		linhas = f.readlines()
 		m,n = linhas[0].split()
-		qtd_devs = int(linhas[int(n)+1])
-		qtd_pos = linhas[qtd_devs+1]
+		n = int(n)
+		print(m, n)
+		qtd_devs = int(linhas[n+1])
+		print(qtd_devs)
+		qtd_pos = int(linhas[qtd_devs+n+2])
+		print(qtd_pos)
+		mapa = linhas[1:n+1]
+		print(mapa)
+		linhas_devs = linhas[n+2:qtd_devs+n+2]
+		print(linhas_devs)
+		linhas_pos = linhas[qtd_devs+n+3::]
+		print(linhas_pos)
 
 
-		print(str_input)
+
+		
 
 
 read_input(sys.argv[1])
