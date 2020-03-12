828cf11cbeb35b10dbfa16ec12c306bb402e69f9
Heitor Pascoal de Bittencourt <heitor@itera.com.br>
2020-03-12 13:33:28 -0300

    add Replyer

---
+++ src/Replyer.py
@@ -0,0 +1,15 @@
+class Replyer:
+    def __init__(self, replyer_d, company, bonus, skills = []):
+        self.id = replyer_id
+        self.company = company
+        self.bonus = bonus
+        self.skills = skills
+        self.dev_friends = []
+        self.po_friends = []
+
+
+    def make_friendship_with_dev(self, dev_id, dev_list):
+        pass
+
+    def make_friendship_with_po(self, po_id):
+        pass
